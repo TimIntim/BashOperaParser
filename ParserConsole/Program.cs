14e36c9e5f3fa8615613940b080705cb7e0e8eea
TimIntim <timintim1995@gmail.com>
2024-09-30 21:15:35 +0500

    initial commit

---
+++ ParserConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+using var client = new HttpClient();
+
+try
+{
+    var url = "https://www.bashopera.ru/affiche/";
+    string htmlContent = await client.GetStringAsync(url);
+    Console.WriteLine(htmlContent);
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine($"Ошибка получения страницы: {e.Message}");
+}
